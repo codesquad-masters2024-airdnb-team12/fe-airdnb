070bf79c08230d02f48ecc9dd80467b13357399c
sharpie1330 <amberaplin14@gmail.com>
2024-06-21 03:28:55 +0900

    feat: NotFound 페이지 구현

--- src/route.svelte
+++ src/route.svelte
@@ -2,6 +2,15 @@
     import { Route } from 'tinro';
     import AccommodationPage from "./page/AccommodationPage.svelte";
     import AccommodationDetail from "./components/AccommodationDetail.svelte";
+    import NotFound from "./page/NotFound.svelte";
+
+    // 여기에 정의된 경로 외에는 모두 NotFound 페이지로 연결됩니다.
+    const validPaths = ['/accommodations', '/', '/register'];
+
+    // 현재 경로가 유효한지 확인하는 함수
+    function isValidPath(path) {
+        return validPaths.includes(path);
+    }
 </script>
 
 <Route path="/" redirect="/accommodations"/>
@@ -9,5 +18,11 @@
 <Route path="/accommodations/:accommodationId" let:params>
     {#if /^\d+$/.test(params.accommodationId)}
         <AccommodationDetail accommodationId={params.accommodationId}/>
+    {:else}
+        <NotFound />
     {/if}
 </Route>
+
+{#if !isValidPath(window.location.pathname)}
+    <NotFound />
+{/if}
